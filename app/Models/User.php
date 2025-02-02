a1ce0e1290bd2b9db6802d36e28a0aea23c20373
laurelis24 <s23kairlaur@venta.lv>
2025-02-02 18:12:24 +0200

    WIP: User roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -33,6 +34,19 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+
+    public function roles(): BelongsToMany
+{
+    return $this->belongsToMany(Role::class);
+}
+
+
+public function hasRole(string $role): bool
+    {
+        return $this->roles()->where('name', $role)->exists();
+    }
+
 
     /**
      * Get the attributes that should be cast.
