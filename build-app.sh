9f258fc1da40c09fdb28dbf2f30f5e191ef3c2a8
laurelis24 <s23kairlaur@venta.lv>
2025-05-15 17:48:30 +0300

    script update

--- build-app.sh
+++ build-app.sh
@@ -14,10 +14,8 @@ npm run build
 php artisan optimize:clear
 
 # Cache the various components of the Laravel application
-php artisan optimize:clear
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
 php artisan view:cache
-
-php artisan db:seed
+php artisan storage:link
